33009af9bc1dea658f5835dcb8c5162b848de2b2
yuanzhi_wang <yuanzhi_wang@163.com>
2017-12-03 08:50:29 +0800

    home2

---
+++ js基础语法/数据类型.js
@@ -0,0 +1,31 @@
+
+//Js有6中数据类型
+//----->基本数据类型
+//-------->String
+//-------->Number
+//-------->Undefined
+//-------->Null
+//-------->Boolean
+//----->复杂数据类型
+//-------->Object
+
+var a = "jasmine";//string
+var b = 1;//number
+var c ;//undefined
+var d = null;//object
+var e = true;//boolean
+
+var f = {name:"jasmine",age:1};//object
+var temp = [1,2,3];//object
+
+console.log(typeof a);//数据类型.js:21 string
+console.log(typeof b);//数据类型.js:22 number
+console.log(typeof c);//数据类型.js:23 undefined
+console.log(typeof d);//数据类型.js:24 object
+console.log(typeof e);//数据类型.js:25 boolean
+console.log(typeof f);//数据类型.js:26 object
+console.log(typeof temp);//数据类型.js:27 object
+
+
+//----->
+//-------->
